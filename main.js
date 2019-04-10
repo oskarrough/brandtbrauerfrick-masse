ea77a08345394adcb51ac5e64f489b56ef6a690d
eve vitrugno <eve@Air-de-eve.fritz.box>
2019-04-10 18:45:45 +0200

    clean css

--- main.js
+++ main.js
@@ -2,3 +2,19 @@
 // only clicked video to have sound, others are muted
 // video encoding on low quality
 
+const videos = document.querySelectorAll("video");
+
+
+videos.forEach(video => {
+    video.addEventListener("click", event =>{
+        if (event.target.paused) {
+            playVideo(event.target);
+            showBox(event.target);
+        }
+        else {
+            pauseVideo(event.target);
+            hideBox(event.target);
+        }
+        
+    })
+});

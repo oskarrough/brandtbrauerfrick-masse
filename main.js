fc91a68b32def948c543834b394e2569c5717c20
Oskar Rough <oskar@rough.dk>
2019-04-11 11:33:13 +0200

    Ready the javascripts!

--- main.js
+++ main.js
@@ -2,19 +2,20 @@
 // only clicked video to have sound, others are muted
 // video encoding on low quality
 
-const videos = document.querySelectorAll("video");
-
+const videos = document.querySelectorAll('video')
 
 videos.forEach(video => {
-    video.addEventListener("click", event =>{
-        if (event.target.paused) {
-            playVideo(event.target);
-            showBox(event.target);
-        }
-        else {
-            pauseVideo(event.target);
-            hideBox(event.target);
-        }
-        
-    })
-});
+  video.addEventListener('click', handleVideoClick)
+})
+
+function handleVideoClick(event) {
+	const paused = event.target.paused
+  console.log({paused})
+  // if (event.target.paused) {
+  //   playVideo(event.target)
+  //   showBox(event.target)
+  // } else {
+  //   pauseVideo(event.target)
+  //   hideBox(event.target)
+  // }
+}

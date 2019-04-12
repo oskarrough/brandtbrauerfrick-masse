c3e43058ed04bd7d2814a5c32a926b86eb75cf60
Oskar Rough <oskar@rough.dk>
2019-04-12 11:17:53 +0200

    Run prettier

--- main.js
+++ main.js
@@ -1,43 +1,43 @@
 const videos = document.querySelectorAll("video");
 
 function muteVideos() {
-    videos.forEach( video => {
-        video.muted = true;
-    })
+	videos.forEach( video => {
+		video.muted = true;
+	})
 }
 
 function playVideos() {
-    videos.forEach(video => {
-        video.play();
-    })   
+	videos.forEach(video => {
+		video.play();
+	})   
 }
 
 function pauseVideos() {
-    videos.forEach(video => {
-        video.pause();
-    })
+	videos.forEach(video => {
+		video.pause();
+	})
 }
 
 function handleVideoClick() {
 
-    if (event.target.paused) {
-    /* target is paused and requested by user */
-        muteVideos();
-        event.target.muted = false;
-        playVideos();
-    }
-    else if (!event.target.paused && event.target.muted){
-        /* while current instrument is played, user requests for another instrument */
-        muteVideos();
-        event.target.muted = false;       
-    }
-    else {
-        /* user pauses currently listened video */
-        muteVideos();
-        pauseVideos();
-    }
+	if (event.target.paused) {
+		/* target is paused and requested by user */
+		muteVideos();
+		event.target.muted = false;
+		playVideos();
+	}
+	else if (!event.target.paused && event.target.muted){
+		/* while current instrument is played, user requests for another instrument */
+		muteVideos();
+		event.target.muted = false;       
+	}
+	else {
+		/* user pauses currently listened video */
+		muteVideos();
+		pauseVideos();
+	}
 }
 
 videos.forEach(video => {
-    video.addEventListener("click", handleVideoClick);
+	video.addEventListener("click", handleVideoClick);
 })

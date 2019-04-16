ec35e898cabc0274de8a72e753ab0c0c1b7a151f
gregory vespasien <vespasiengregory@gmail.com>
2019-04-16 12:17:45 +0200

    issue in main's render(), it does not render the video customs elements

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 import {VideoCustom} from '/video_custom.js'
-import {default as videosIDs} from '/model.js'
+import videosIDs from '/model.js'
 
 const hyper = window.hyperHTML;
 const grid = document.querySelector(".GridOrchestra");
@@ -14,28 +14,22 @@ class View {
   }
 
   render() {
-
-    grid.appendChild(hyper()`<div class="GridOrchestra-box GridOrchestra-box--big"></div>`);
-
-    videosIDs.forEach(video => {
-        grid.appendChild(hyper()`<video-custom id=${video.id}></video-custom>`)
-    });
-    
-    const credits = hyper()`
-                        <div class="GridOrchestra-box CreditsBox">
-                          <h1 class="Title">Brandt Brauer Frick's <i>Masse</i></h1>
-                          <p>Click on any clip to listen to solo voices</p>
-                          <p><i>Masse</i> is taken from Brand Brauer Frick's <i>Echo</i> album, out on May 31st 2019</p>
-                          <a href="www.google.com">Credits</a>
-                        </div>   
-                        `;
-    grid.appendChild(credits);
-    
+    hyper(grid)`
+      <div class="GridOrchestra-box GridOrchestra-box--big"></div>
+      ${this.state.ids.forEach(video => 
+        hyper()`<video-custom id=${video.id}></video-custom>`
+      )}
+      <div class="GridOrchestra-box CreditsBox">
+        <h1 class="Title">Brandt Brauer Frick's <i>Masse</i></h1>
+        <p>Click on any clip to listen to solo voices</p>
+        <p><i>Masse</i> is taken from Brand Brauer Frick's <i>Echo</i> album, out on May 31st 2019</p>
+        <a href="www.google.com">Credits</a>
+      </div>   
+    `;
     grid.querySelectorAll("video").forEach(video => {
             video.addEventListener("click", this.handleVideoClick.bind(this),false);
     });
   }
-
   handleVideoClick() {
       if (event.target.paused) {
         /* target is paused and requested by user */
@@ -61,7 +55,6 @@ class View {
        this.pauseVideos();
       }
   }
-  
   muteVideos() {
     grid.querySelectorAll("video").forEach(video => {
       video.muted = true;

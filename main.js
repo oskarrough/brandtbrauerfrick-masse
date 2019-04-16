6c0d546658f254ab397d3c381b47cd60b4778719
gregory vespasien <vespasiengregory@gmail.com>
2019-04-16 13:16:28 +0200

    delete model

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ import {VideoCustom} from '/video_custom.js'
 import videosIDs from '/model.js'
 
 const hyper = window.hyperHTML;
+const grid = document.querySelector(".GridOrchestra");
 customElements.define("video-custom", VideoCustom);
 
 class View {

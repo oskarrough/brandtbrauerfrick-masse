2423cfef9fcd13162e8b238abea5066e2fdd69ca
Oskar Rough <oskar@rough.dk>
2019-05-02 19:52:36 +0200

    Switch to use videos from Vimeo

--- video_custom.js
+++ video_custom.js
@@ -1,34 +1,42 @@
 const hyper = window.hyperHTML
 
 export class VideoCustom extends HTMLElement {
-  constructor() {
-    super()
-    this.html = hyper(this)
-    this.state = {}
-  }
-  connectedCallback() {
-    this.classList.add('GridOrchestra-grid-box')
-    this.state.id = this.id
-    this.state.title = this.getAttribute('data-title')
-    this.render()
-  }
-  render() {
-    const baseURL = 'https://res.cloudinary.com/bbf/video/upload/'
-    const encodingImages = 'f_auto,q_auto'
-    const encodingH265 = 'vc_h265,w_1280,c_limit'
-    const encodingVP9 = 'vc_vp9,w_1280,c_limit'
-    const encodingAuto = 'vc_auto,w_1280,c_limit'
-    const urlPoster = baseURL + encodingImages + '/' + this.state.id + '.jpg'
-    const urlH265 = baseURL + encodingH265 + '/' + this.state.id + '.mp4'
-    const urlVP9 = baseURL + encodingVP9 + '/' + this.state.id + '.webm'
-    const urlAuto = baseURL + encodingAuto + '/' + this.state.id + '.mp4'
-    this.html`
-      <span class="ActiveDot"></span>
+	constructor() {
+		super()
+		this.html = hyper(this)
+		this.state = {}
+	}
+	connectedCallback() {
+		this.render()
+	}
+	render() {
+		const src = this.getAttribute('src')
+		const id = this.getAttribute('cloudinary-id')
+		const baseURL = 'https://res.cloudinary.com/bbf/video/upload/'
+		const encodingImages = 'f_auto,q_auto'
+		const encodingH265 = 'vc_h265,w_1280,c_limit'
+		const encodingVP9 = 'vc_vp9,w_1280,c_limit'
+		const encodingAuto = 'vc_auto,w_1280,c_limit'
+		const urlPoster = baseURL + encodingImages + '/' + id + '.jpg'
+		const urlH265 = baseURL + encodingH265 + '/' + id + '.mp4'
+		const urlVP9 = baseURL + encodingVP9 + '/' + id + '.webm'
+		const urlAuto = baseURL + encodingAuto + '/' + id + '.mp4'
+
+		// If present, prefer normal video src to Cloudinary id.
+		if (src) {
+			return this.html`
+				<span class="ActiveDot"></span>
+				<video src=${src} preload="auto" poster=${urlPoster}></video>
+			`
+		}
+
+		this.html`
+			<span class="ActiveDot"></span>
 			<video preload="auto" poster=${urlPoster}>
 				<source src=${urlH265} type="video/mp4; codecs=hvc1">
 				<source src=${urlVP9} type="video/webm; codecs=vp9">
 				<source src=${urlAuto} type="video/mp4">
 			</video>
 		`
-  }
+	}
 }

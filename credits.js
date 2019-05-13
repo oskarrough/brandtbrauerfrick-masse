359b404a63d2c3dc8f428e62ca36f2be4774059e
Gregory Vespasien <evevitrugno@Airdeevvitrugno.fritz.box>
2019-05-13 20:34:33 +0200

    tweaks overlay

--- credits.js
+++ credits.js
@@ -1,17 +1,13 @@
 // credits overlay logic
 
 function closeCredits() {
-    elements.overlay.classList.remove("is-open");
-    elements.overlayContent.classList.remove("is-active");
-    elements.overlayBackdrop.classList.remove("is-active");
+    elements.overlay.classList.remove("is-active");
     elements.closeOverlayBtn.classList.remove("is-active");
     document.removeEventListener("keydown", handleKeyDown);
 }
 
 function openCredits() {
-    elements.overlay.classList.add("is-open");
-    elements.overlayContent.classList.add("is-active");
-    elements.overlayBackdrop.classList.add("is-active");
+    elements.overlay.classList.add("is-active");
     elements.closeOverlayBtn.classList.add("is-active");
     document.addEventListener("keydown", handleKeyDown);
 }
@@ -25,7 +21,6 @@ function handleKeyDown(event) {
 const elements = {
   overlay: document.querySelector(".CreditsOverlay"),
   overlayBackdrop: document.querySelector(".CreditsOverlay-backdrop"),
-  overlayContent: document.querySelector(".CreditsOverlay-content"),
   closeOverlayBtn: document.querySelector(".CreditsOverlay-button"),
   openOverlayBtn: document.querySelector(".Credits")
 }

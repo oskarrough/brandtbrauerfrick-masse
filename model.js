d5ec278021ff23d33497ebd39d7486d8bff33915
danielobrando <50925205+danielobrando@users.noreply.github.com>
2019-06-11 11:44:49 +0100

    Update model.js

--- model.js
+++ model.js
@@ -1,25 +1,97 @@
 export const tourData = [
-	{
-		date: 'May 24th',
-		location: 'Nova Mix Club at Badaboum - Paris, FR',
-		link: 'http://badaboum.paris/event/novamixclub-10/'
-	},
-
-	{
+		{
 		date: 'June 14th',
 		location: 'Cinemateca - Bogota, CO',
-		link: ''
+		link: 'https://www.goethe.de/prj/hya/es/ver.cfm?fuseaction=events.detail&event_id=21590345'
 	},
 
 	{
 		date: 'June 16th',
 		location: 'El Dragon De Barranco - Lima, PE',
-		link: ''
+		link: 'https://www.goethe.de/prj/hya/es/ver.cfm?fuseaction=events.detail&event_id=21590395'
+	},
+
+	{
+		date: 'July 19th',
+		location: 'Somerset House - London, UK',
+		link: 'https://www.somersethouse.org.uk/whats-on/somerset-house-summer-series/cut-copy'
+	},
+
+	{
+		date: 'July 27th',
+		location: 'Heimspiel Knyphausen - Eltville, DE',
+		link: 'http://www.heimspiel-knyphausen.de/'
 	},
 
 	{
 		date: 'August 10th',
 		location: 'Haldern Pop Festival - Haldern, DE',
 		link: 'http://haldernpop.com/band/brandt-brauer-frick-de/'
+	},
+
+	{
+		date: 'October 18th',
+		location: 'L’Aéronef (30th Anniversary) - Lille, FR',
+		link: 'http://aeronef.fr/'
+	},
+
+	{
+		date: 'October 25th',
+		location: 'Kaltern Pop - Kaltern, IT',
+		link: 'https://kalternpop.de/tickets-2019/'
+
+	},
+
+	{
+		date: 'November 3rd',
+		location: 'Volksbühne - Berlin, DE',
+		link: 'https://www.volksbuehne.berlin/de/programm/8605/brandt-brauer-frick'
+
+	},
+
+	{
+		date: 'November 8th',
+		location: 'UT Connewitz - Leipzig, DE',
+		link: 'https://www.tixforgigs.com/site/Pages/Shop/ShowEvent.aspx?ID=30790&af=ozqw0atnumoi&saf=ea7muyp'
+
+	},
+
+	{
+		date: 'November 9th',
+		location: 'Kleinvieh - Dresden, DE',
+		link: 'https://www.tixforgigs.com/site/Pages/Shop/ShowEvent.aspx?ID=30805'
+
+	},
+
+	{
+		date: 'November 10th',
+		location: 'Enjoy Jazz Festival - Heidelberg, DE',
+		link: 'https://enjoyjazz.reservix.de/tickets-brandt-brauer-frick-in-heidelberg-kulturhaus-karlstorbahnhof-am-10-11-2019/e1395926'
+	},
+
+	{
+		date: 'November 13th',
+		location: 'Import / Export - Munich, DE',
+		link: 'https://clubzwei.reservix.de/p/reservix/event/1418623'
+	},
+
+	{
+		date: 'November 14th',
+		location: 'Stadtgarten - Cologne, DE',
+		link: 'https://www.eventim.de/noapp/tickets.html?fun=evdetail&affiliate=EVE&doc=evdetailb&key=1103186%2412022774'
+	},
+
+	{
+		date: 'November 15th',
+		location: 'Christuskirche - Bochum, DE',
+		link: 'https://christuskirche-bochum.reservix.de/p/reservix/group/311334'
+	},
+
+	{
+		date: 'November 16th',
+		location: 'Mojo - Hamburg, DE',
+		link: 'https://www.eventim.de/tickets.html?affiliate=EVE&doc=evdetailb&key=12028523&erid=1103186&sort_by=event_datum&sort_direction=asc#ticketTable'
+
+
 	}
 ]

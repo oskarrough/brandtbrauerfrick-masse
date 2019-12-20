e48622a2329946950bf94ddf67b6f2359c41a0fb
danielobrando <50925205+danielobrando@users.noreply.github.com>
2019-12-20 07:41:44 +0700

    Update model.js

--- model.js
+++ model.js
@@ -1,70 +1,43 @@
 export const tourData = [
 	{
-		date: 'October 17th',
-		location: 'Point Ephémère - Paris, FR',
+		date: 'January 31st',
+		location: 'Sala Simón Bolívar (w/ El Sistema Orchestra) - Caracas, VE',
 		link:
-			'https://web.digitick.com/brandt-brauer-frick-concert-point-ephemere-paris-17-octobre-2019-css5-supermonamour-pg101-ri6309517.html'
+			'https://sistemaglobal.org/'
 	},
 
-	{
-		date: 'October 18th',
-		location: 'L’Aéronef (30th Anniversary) - Lille, FR',
-		link: 'http://aeronef.fr/'
-	},
-
-	{
-		date: 'October 24th',
-		location: 'Kaltern Pop - Kaltern, IT',
-		link: 'https://kalternpop.de/tickets-2019/'
-	},
-
-	{
-		date: 'November 3rd',
-		location: 'Volksbühne - Berlin, DE',
-		link: 'https://www.volksbuehne.berlin/de/programm/8605/brandt-brauer-frick'
-	},
-
-	{
-		date: 'November 8th',
-		location: 'UT Connewitz - Leipzig, DE',
-		link: 'https://www.tixforgigs.com/site/Pages/Shop/ShowEvent.aspx?ID=30790&af=ozqw0atnumoi&saf=ea7muyp'
-	},
-
-	{
-		date: 'November 9th',
-		location: 'Kleinvieh - Dresden, DE',
-		link: 'https://www.tixforgigs.com/site/Pages/Shop/ShowEvent.aspx?ID=30805'
-	},
-
-	{
-		date: 'November 10th',
-		location: 'Enjoy Jazz Festival - Heidelberg, DE',
+{
+		date: 'May 8th',
+		location: 'Tempodrom (Pitchfork Festival) - Berlin, DE',
 		link:
-			'https://enjoyjazz.reservix.de/tickets-brandt-brauer-frick-in-heidelberg-kulturhaus-karlstorbahnhof-am-10-11-2019/e1395926'
+			'https://www.pitchforkmusicfestival.de/'
 	},
 
-	{
-		date: 'November 13th',
-		location: 'Import / Export - Munich, DE',
-		link: 'https://clubzwei.reservix.de/p/reservix/event/1418623'
-	},
-
-	{
-		date: 'November 14th',
-		location: 'Stadtgarten - Cologne, DE',
-		link: 'https://www.eventim.de/noapp/tickets.html?fun=evdetail&affiliate=EVE&doc=evdetailb&key=1103186%2412022774'
-	},
-
-	{
-		date: 'November 15th',
-		location: 'Christuskirche - Bochum, DE',
-		link: 'https://christuskirche-bochum.reservix.de/p/reservix/group/311334'
-	},
-
-	{
-		date: 'November 16th',
-		location: 'Mojo - Hamburg, DE',
+{
+		date: 'May 14th',
+		location: 'Bundeskunsthalle (Ensemble Show) - Bonn, DE',
 		link:
-			'https://www.eventim.de/tickets.html?affiliate=EVE&doc=evdetailb&key=12028523&erid=1103186&sort_by=event_datum&sort_direction=asc#ticketTable'
+			'https://www.bonnticket.de/brandt+brauer+frick+ensemble+-+bundeskunsthalle+-+live+arts-ticket-7480/?evid=2190712&referer_info=&tId=&forwardingTicket=&pageId=7480'
+	},
+
+{
+		date: 'May 15th',
+		location: 'Mozartfest (Ensemble Show) - Augsburg, DE',
+		link:
+			'https://www.mozartstadt.de/'
+	},
+
+{
+		date: 'May 16th',
+		location: 'RWTH (Ensemble Show) - Aachen, DE',
+		link:
+			''
+	},
+
+{
+		date: 'August 1st',
+		location: 'Detect Classic Festival - Neubrandenburg, DE',
+		link:
+			'https://detectclassicfestival.de/'
 	}
 ]

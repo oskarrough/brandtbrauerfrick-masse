3eb0f60ac62b66ef9c30d54b39f2b364d95997ef
danielobrando <50925205+danielobrando@users.noreply.github.com>
2020-09-12 12:55:11 +0100

    Update model.js

--- model.js
+++ model.js
@@ -2,7 +2,7 @@ export const tourData = [
 	
 	
 		{
-		date: 'October 10th',
+		date: 'October 10th 2020',
 		location: 'Resonanzen Festival - Saarbruecken, DE',
 		link:
 			'https://resonanzenfestival.de/project/brandt-brauer-frick-de'
@@ -10,14 +10,14 @@ export const tourData = [
 
 
 {
-		date: 'October 9th',
+		date: 'October 9th 2020',
 		location: 'Bundeskunsthalle - Bonn, DE',
 		link:
 			'https://www.bonnticket.de/brandt+brauer+frick+ensemble+-+bundeskunsthalle+-+live+arts-ticket-7480/?evid=2190712&referer_info=&tId=&forwardingTicket=&pageId=7480'
 	},
 
 {
-		date: 'November 1st',
+		date: 'November 1st 2020',
 		location: 'Strings Of Autumn Festival - Pargue, CZ',
 		link:
 			'https://strunypodzimu.cz/en/brandt-brauer-frick-ensemble-en/'
@@ -26,7 +26,7 @@ export const tourData = [
 
 
 {
-		date: 'December 19th',
+		date: 'December 19th 2020',
 		location: 'RWTH - Aachen, DE',
 		link:
 			'https://www.staedteregion-aachen.de/de/navigation/aemter/kultur-s-16/kulturfestival-x-2020/brandt-brauer-frick-ensemble-16-mai/'
